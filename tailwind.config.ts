f3166a1b738c0c1f876601870b5d092c0b0f9a94
Murray Leys <ml@blueskimo.co.uk>
2023-07-04 14:47:11 +0100

    fixed error in tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,24 @@
-import { type Config } from "tailwindcss";
+// import { type Config } from "tailwindcss";
+//
+// export default {
+//   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// } satisfies Config;
 
-export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config;
+};
